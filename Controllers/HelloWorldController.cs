b5b2e2bff42b60b518e5d9d248e274f1ff7f933d
Jack Ferguson <jack.ferguson552@gmail.com>
2019-10-05 14:55:33 +0100

    modify welcome

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -13,12 +13,9 @@ namespace MvcMovie.Controllers
             return "This is my default action...";
         }
 
-        // 
-        // GET: /HelloWorld/Welcome/ 
-
-        public string Welcome()
+        public string Welcome(string name, int ID = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
